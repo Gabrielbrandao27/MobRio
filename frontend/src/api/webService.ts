a6843e6f3feb8008a9b4e6fa1620897e69b55998
Gabrielbrandao27 <gafbrandao@id.uff.br>
2025-05-07 23:52:14 -0300

    adicionando frontend no projeto

---
+++ frontend/src/api/webService.ts
@@ -0,0 +1,123 @@
+import axiosInstance from "./axiosConfig.ts";
+
+export const landingPage = async () => {
+  try {
+    const response = await axiosInstance.get(`/`);
+    return response.data;
+  } catch (error) {
+    console.error("Error on landing page:", error);
+    throw error;
+  }
+};
+
+export const homePage = async () => {
+  try {
+    const response = await axiosInstance.get(`/home`);
+    return response.data;
+  } catch (error) {
+    console.error("Error on home page:", error);
+    throw error;
+  }
+};
+
+export const registerPage = async (
+  name: string,
+  password: string,
+  email: string
+) => {
+  try {
+    const payload = {
+      name,
+      password,
+      email,
+    };
+    const response = await axiosInstance.post(`/register`, payload);
+    return response.data;
+  } catch (error) {
+    console.error("Error on register page:", error);
+    throw error;
+  }
+};
+
+export const loginPage = async (email: string, password: string) => {
+  try {
+    const payload = {
+      email,
+      password,
+    };
+    const response = await axiosInstance.post(`/login`, payload);
+    localStorage.setItem("token", response.data.access_token);
+    return response.data;
+  } catch (error) {
+    console.error("Error on landing page:", error);
+    throw error;
+  }
+};
+
+export const userBusRelation = async (
+  routeStopId: number,
+  openTime: string,
+  closeTime: string
+) => {
+  try {
+    const payload = {
+      routeStopId,
+      openTime,
+      closeTime,
+    };
+    const response = await axiosInstance.post(`/user_bus_relation`, payload, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error posting user bus relation:", error);
+    throw error;
+  }
+};
+
+export const busRoutes = async () => {
+  try {
+    const response = await axiosInstance.post(`/bus/routes`, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching bus routes:", error);
+    throw error;
+  }
+};
+
+export const busStops = async (routeId: string, directionId: string) => {
+  try {
+    const response = await axiosInstance.get(
+      `/bus/stops?route_id=${routeId}&direction_id=${directionId}`,
+      {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      }
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching bus stops:", error);
+    throw error;
+  }
+};
+
+export const busLivePositions = async () => {
+  try {
+    const response = await axiosInstance.post(`/bus/live_positions`, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching bus live positions:", error);
+    throw error;
+  }
+};
